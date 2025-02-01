d4bf8b456677f66c2490a171d712b6100fb45307
daincoding <official.toyou@gmail.com>
2025-02-01 15:14:52 +0100

    Background Set

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { useState } from 'react'
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <div className='fixed top-0 -z-10 h-full w-full'>
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        <div className="relative h-full w-full bg-slate-950">
-          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-        </div>
-      </div>
-      </div>
+    <div class="relative h-screen">
+  <div class="absolute inset-0">
+    <div class="relative h-full w-full bg-neutral-900 [&>div]:absolute [&>div]:inset-0 [&>div]:bg-fuchsia-400 [&>div]:bg-[size:20px_20px] [&>div]:opacity-20 [&>div]:blur-[100px]">
+    <div></div>
+  </div>
+  </div>
+    </div>
   )
 }
 
